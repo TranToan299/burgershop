ead4d017c438e023fc349366aa825389a2850346
TranToan299 <toantran58.k44@gmail.com>
2022-12-06 23:32:09 +0700

    final

--- src/Component/Burger.jsx
+++ src/Component/Burger.jsx
@@ -1,30 +1,129 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const Burger = () => {
+  const [total, setTotal] = useState(80)
   const stateBurger = useSelector((state) => state.burger);
   const stateMenu = useSelector((state) => state.menu);
   const dispatch = useDispatch()
-  console.log(stateMenu, stateBurger);
+  let renderBurgerMid = () => {
+    let content = [];
+    for (let propBurger in stateBurger) {
+      let breadMid = []
+      for (let i = 0; i < stateBurger[propBurger]; i++) {
+        breadMid.push(<div className={propBurger} key={i}></div>)
+      }
+      content.push(breadMid)
+    }
+    return content;
+    // Cách 2
+    // let arrBuger = Object.entries(stateBurger)
+    // return arrBuger.map(([propsBurger,value],index) => {
+    //   let breadMid = [];
+    //   for (let i = 0; i < value; i++){
+    //     breadMid.push(<div className={propsBurger} key = {i}></div>)
+    //   }
+    //   return breadMid;
+    // })
+  }
+  useEffect( () => {
+    setTotal((total) => {
+      let arrTotal = []
+      for (let props in stateBurger) {
+        let totalProps = stateBurger[props]*stateMenu[props]
+       arrTotal.push(totalProps)
+        // total += totalProps;
+      }
+     total =  arrTotal.reduce((total,item) => {
+        return total + item
+      })
+    
+      console.log(arrTotal)
+      return total
+    })
+  },[stateBurger])
+
+
+
+  const renderBeugerMenu = () => {
+      let arrMenu = Object.entries(stateMenu).map(([propItem,value],index) => {
+        
+        return  <tr key = {index}>
+        <td>{propItem}</td>
+        <td>
+          <button className="btn btn-success mx-3" onClick = {() => {
+            const action = {
+              type:'TANG_GIAM_SO_LUONG',
+              propItem,
+              value,
+              payload: 1 
+            }
+            dispatch(action)
+          }}>+</button>
+            {stateBurger[propItem]}
+          <button className="btn btn-danger mx-3" onClick = {() => {
+            const action = {
+              type:'TANG_GIAM_SO_LUONG',
+              propItem,
+              value,
+              payload: -1
+            }
+            dispatch(action)
+          }}>-</button>
+        </td>
+        <td>
+        {value}
+        </td>
+        <td>{value * stateBurger[propItem] }</td>
+      </tr>
+      })
+      return arrMenu
+     
+
+  }
+ 
+
+
+
+
   return (
     <div className="container">
-        <h2 className="text-center">Bài tập Burger</h2>
+      <h2 className="text-center">Bài tập Burger</h2>
       <div className="row">
         <div className="col-6">
           <div className="burger-left">
             <div className="breadTop"></div>
-            <div className="salad"></div>
+            {/* <div className="salad"></div>
             <div className="beef"></div>
-            <div className="cheese"></div>
+            <div className="cheese"></div> */}
+            {renderBurgerMid()}
             <div className="breadBottom"></div>
           </div>
         </div>
         <div className="col-6">
-            <div className="burger-right">
+          <div className="burger-right">
+            <h3 className="text-center text-success">Chọn thức ăn</h3>
             <table className="table">
-        
+              <thead>
+                <tr>
+                  <th>Thức ăn</th>
+                  <th></th>
+                  <th>Đơn giá</th>
+                  <th>Thành tiền</th>
+
+                </tr>
+
+              </thead>
+              <tbody>
+              {renderBeugerMenu()}
+            <tr>
+            <td colSpan = {3}></td>
+            <td>{total}</td>
+
+            </tr>
+              </tbody>
             </table>
-            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Component/configStore.jsx
+++ src/Component/configStore.jsx
@@ -1,14 +1,28 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 
 export const store = configureStore({
-    reducer:{
-        burger: (state ={salad:1,beef:1}) => {
+    reducer: {
+        burger: (state = { salad: 1, beef: 1, cheese: 5 }, action) => {
+            switch (action.type) {
+                case 'TANG_GIAM_SO_LUONG': {
+                    let burgerUpdate = {...state};
+                    burgerUpdate[action.propItem] += action.payload;
+                    if (burgerUpdate[action.propItem] < 1) {
+                        return {...state}
+                    }
+                    
+                    console.log(burgerUpdate)
+                    state = burgerUpdate;
+                    return {...state};
+                }
+                default: {return state}
+            }
+           
+        },
+        menu: (state = { salad: 10, beef: 55, cheese: 20 }, action) => {
             return state
         },
-        menu: (state = {salad:10,beef:20}) => {
-            return state
-        }
 
     }
 })
